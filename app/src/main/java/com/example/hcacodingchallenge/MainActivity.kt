af9de1522d4542f74feaa281bf5e4453ba223f5c
taylorbounds <boundstaylor@gmail.com>
2022-02-01 21:22:01 -0500

    first commit

---
+++ app/src/main/java/com/example/hcacodingchallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hcacodingchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_stackexchange_list_item)
+    }
+}
